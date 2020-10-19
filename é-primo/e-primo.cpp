27b65660c62a96e5c6a182a7e1bdf2fb47e70b64
gabriellucascm <63599233+gabriellucascm@users.noreply.github.com>
2020-10-18 22:58:59 -0300

    Update e-primo.cpp

--- é-primo/e-primo.cpp
+++ é-primo/e-primo.cpp
@@ -8,8 +8,8 @@ int main(int argc, char const *argv[]) {
   int n,primo=1; //inicializo o número a ser analisado e um "verificador", que sera igual a 1 enquanto
                  //nao encontrarmos um número divisível por ele
   cin>>n;
-  for(int i=2;i<=n/2;i++){//comeco o for a partir de 2, pois 1 é divide todos os numeros e 0 não pode dividir nada
-                          //irei continuar o loop até n/2, pois a metade de um número é o seu maior divisor possível
+  for(int i=2;i*i<=n;i++){//comeco o for a partir de 2, pois 1 é divide todos os numeros e 0 não pode dividir nada
+                          //irei continuar o loop até raiz de n
     if(n%i==0){
       primo=0;//caso o módulo de n por i (resto da divisao de n por i) for igual a zero, i é um divisor de n
     }         //logo n não é primo
